566a574b666165d833b90583c12b31cb8c43c553
miaocunfa <42854918+miaocunfa@users.noreply.github.com>
2020-01-16 17:40:38 +0800

    Update bklog.sh

--- log/bklog.sh
+++ log/bklog.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+#-------------------
+# Function
+#-------------------
+
 exit_handler()
 {
     exit $EXITCODE
 }
 
-##############
-# Main
-##############
+bklog()
+{
+    if [ ! -d $WORKDIR/oldlogs/$logshort/$TODAY ]
+    then
+        mkdir -p $WORKDIR/oldlogs/$logshort/$TODAY
+    fi
+                
+    SEQ=$((`ls -l ${WORKDIR}/oldlogs/$logshort/$TODAY/$logshort.$TODAY.[0-9]* 2> /dev/null | wc -l`))
+    OUTFILE="${WORKDIR}/oldlogs/$logshort/$TODAY/$logshort.$TODAY.$SEQ"
+}
 
-##################
+#-------------------
 # GLOBAL VARIABLES
-##################
+#-------------------
+
 ENVFILE="$HOME/.bash_profile"
 
 YEAR=`/bin/date +"%Y"`
@@ -24,9 +36,9 @@ EXITCODE=0
 
 WORKDIR="/opt/aihangxunxi/logs"
 
-####################################
+#----------------------------
 # Load the environment file 
-####################################
+#----------------------------
 
 if [ -r "$ENVFILE" ]
 then
@@ -36,9 +48,9 @@ else
     exit_handler
 fi
 
-########################################
-# Reset and save the current trace files
-########################################
+#-----------------------------------------
+# Reset and save the current log files
+#-----------------------------------------
 
 cd $WORKDIR
 
@@ -47,23 +59,35 @@ then
     mkdir oldlogs
 fi
 
-for logfile in `ls info*.log`
-do
-    if [ -s $logfile ]
-	then
-        logshort=`echo $logfile | awk -F. '{print $1}'`
+filespec=$1
 
-        if [ ! -d $WORKDIR/oldlogs/$logshort/$TODAY ]
-        then
-            mkdir -p $WORKDIR/oldlogs/$logshort/$TODAY
-        fi
-        
-        SEQ=$((`ls -l ${WORKDIR}/oldlogs/$logshort/$TODAY/$logshort.$TODAY.[0-9]* 2> /dev/null | wc -l`))
-        OUTFILE="${WORKDIR}/oldlogs/$logshort/$TODAY/$logshort.$TODAY.$SEQ"
+if [ -n "$filespec" ]
+then
+    if [ -f $filespec ]
+    then
+        logshort=`echo $filespec | awk -F. '{print $1}'`
 
-        mv $logfile $OUTFILE
-        zip -m $OUTFILE.zip $OUTFILE
+        bklog
+        cp $filespec $OUTFILE
+        >$filespec
+        zip -m $OUTFILE.zip $OUTFILE 
+    else
+        echo "$filespec: No such file or directory"
+        exit_handler
     fi
-done
+else
+    for logfile in `ls info*.log`
+    do
+        if [ -s $logfile ]
+        then
+            logshort=`echo $logfile | awk -F. '{print $1}'`
+
+            bklog
+            cp $logfile $OUTFILE
+            >$logfile
+            zip -m $OUTFILE.zip $OUTFILE
+        fi
+    done
+fi
 
 exit_handler
