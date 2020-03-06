34bc5dd7322d88b49daefd01037b74fbd744e505
miaocunfa <42854918+miaocunfa@users.noreply.github.com>
2020-03-06 14:14:02 +0800

    Create deploy_new_inns.sh

---
+++ deploy_new_inns.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+#-------------------
+# Function
+#-------------------
+
+exit_handler()
+{
+    exit $EXITCODE
+}
+
+#-------------------
+# GLOBAL VARIABLES
+#-------------------
+
+ENVFILE="$HOME/.bash_profile"
+
+curDate=`date +'%Y%m%d'`
+curTime=`date +'%H%M%S'`
+
+EXITCODE=0
+
+DeployDIR=(
+    /home/miaocunfa
+    /home/ahzll
+)
+
+WorkDIR=/ahdata/www
+Zipfile=aihanginns.zip
+Unzipfile=aihanginns
+CurRelease=www.aihanginns.com
+
+#----------------------------
+# Load the environment file 
+#----------------------------
+
+if [ -r "$ENVFILE" ]
+then
+    . $ENVFILE
+else
+    EXITCODE=-1
+    exit_handler
+fi
+
+#-----------------------------------------
+# Deploy new release and backup
+#-----------------------------------------
+
+cd $WorkDIR
+
+if [ ! -d Release ]
+then
+    mkdir Release
+fi
+
+for i in "${DeployDIR[@]}";
+do
+    if [ -f $i/$Zipfile ]
+    then
+        mv $i/$Zipfile $WorkDIR
+        unzip -o $Zipfile
+        rm -rf __MACOSX
+
+        mv $WorkDIR/$CurRelease Release/$CurRelease.bak$curDate$curTime
+        mv $Unzipfile $CurRelease
+
+        echo "Deploy Successfully!"
+
+        exit_handler
+    else
+        echo
+        echo "$i/$Zipfile        is not found!"
+    fi
+done
+
+echo
+exit_handler
