4ebbd9de4fdcb5b4dc278d337e9d4ee1be5b2fc0
miaocunfa <42854918+miaocunfa@users.noreply.github.com>
2020-04-01 18:29:56 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,92 +3,121 @@
 #===================================================================
 ENVFILE="/etc/profile"
 EXITCODE=0
-curDate=`date +'%Y%m%d'`
-curTime=`date +'%H%M%S'`
+curDate=$(date +'%Y%m%d')
+curTime=$(date +'%H%M%S')
 
+workPath=/opt/aihangxunxi
+worklibPath=$workPath/lib
+
+tcpMessageSleep=1
 tcpMessagePort=8555
-tcpMessagePortSleep=2
+tcpMessageSock=9666
+tcpMessagePortNum=1
+tcpMessageSocktNum=1
 messagelib=info-message-service.jar
 
 deployPath=/home/miaocunfa
-workPath=/opt/aihangxunxi
-worklibPath=$workPath/lib
-full_service_file=$workPath/bin/deploy.service
+deploy_ps_sleep=1
+deploy_ps_num=1
+deploy_ps_file=$workPath/bin/.deploy.ps.service
 
-full_service=${full_service:-default}
-service_num=${service_num:-default}
-success_num=${success_num:-default}
-failed_num=${failed_num:-default}
+total_service_file=$workPath/bin/.total.service
+total_service_ps_file=$workPath/bin/.total.service.ps
+total_service=${total_service:-default}
+service_num=0
+success_num=0
+failed_num=0
 
 #===================================================================
 cd $deployPath
 
-full_service=`ls *.jar`
-
->$full_service_file
-echo $full_service >> $full_service_file
+total_service=$(ls *.jar)
+echo $total_service >> $total_service_file
 
 echo
 echo "ls $deployPath"
-echo "$full_service"
+echo "$total_service"
 echo
 
-echo $full_service | grep -wq "$messagelib" && isMessage="0" || isMessage="1"
+echo $total_service | grep -wq "$messagelib" && isMessage="0" || isMessage="1"
 
 if [ $isMessage == "0" ]
 then
     cd $workPath/bin
-    cur_datetime=`date +'%Y%m%d%H%M%S'`
+    cur_datetime=$(date +'%Y%m%d%H%M%S')
 
     ./stop.sh $messagelib
     
     mv $worklibPath/$messagelib    $worklibPath/$messagelib.$cur_datetime
     mv $deployPath/$messagelib     $worklibPath/$messagelib
 
-
-    tcpMessagePortNum=$(ss -an | grep $tcpMessagePort | awk '$1 == "tcp" && $2 == "LISTEN" {print $0}' | wc -l)
     echo
-    echo "waiting for message connection close..."
+    echo "Waiting for MessageService Port Connection Close!"
     echo
 
-
-    while [ $tcpMessagePortNum -ge 1 ]
+    while ( [ $tcpMessagePortNum -ge 1 ] || [ $tcpMessageSocktNum -ge 1 ] )
     do
-        sleep $tcpMessagePortSleep
+        sleep $tcpMessageSleep
         tcpMessagePortNum=$(ss -an | grep $tcpMessagePort | awk '$1 == "tcp" && $2 == "LISTEN" {print $0}' | wc -l)
+        tcpMessageSocktNum=$(ss -an | grep $tcpMessageSock | awk '$1 == "tcp" && $2 == "LISTEN" {print $0}' | wc -l)
     done
     
+    sleep 2
     ./start.sh $messagelib
 fi
 
-full_service=`ls $deployPath`
+total_service=$(ls $deployPath)
 
 echo
 echo "ls $deployPath"
-echo "$full_service"
+echo "$total_service"
 echo
 
-for i in `ls $deployPath`
+for i in $(ls $deployPath)
 do
     cd $workPath/bin
-    cur_datetime=`date +'%Y%m%d%H%M%S'`
+    cur_datetime=$(date +'%Y%m%d%H%M%S')
 
     ./stop.sh $i
 
     mv $worklibPath/$i    $worklibPath/$i.$cur_datetime
     mv $deployPath/$i     $worklibPath/$i
 
+    while [ $deploy_ps_num -ge 1 ]
+    do
+        sleep $deploy_ps_sleep
+        deploy_ps_num=$(ps -ef | grep $i | grep -v "grep" | wc -l)
+    done
+
+    sleep 2
     ./start.sh $i
 done
 
-for i in `cat $full_service_file`
+for i in $(cat $total_service_file)
 do
+    ps -ef| grep $i | grep -v "grep" >> $total_service_ps_file
 
+    if [ -s $total_service_ps_file ]
+    then
+        success_num=$(( $success_num + 1 ))
+    else
+        failed_num=$(( $failed_num + 1 ))
+    fi
+
+    cat $total_service_ps_file
+    echo
+    >$total_service_ps_file
 done
 
-ps -ef| grep info-message | grep -v "grep"
+service_num=$(awk '{print NF}' $total_service_file)
 
-service_num=``
+echo
+printf "\tTotal [%02i] Service are Deploy\n" $service_num
+printf "\tSuccessfully [%02i], Failed [%02i]\n" $success_num $failed_num
+echo
+printf "\tHave a good Day, see you later"
+echo
 
-echo "Total $service_num Service are Deploy!"
-echo "Successfully $success_num    Failed $failed_num"
+>$total_service_ps_file
+>$total_service_file
+>$deploy_ps_file
